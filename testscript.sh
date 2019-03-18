3ce9d76fd60e9d75dcad1488380d108ecf88193d
B J Bhatt <bhuvnesh.bhatt@nih.gov>
2019-03-18 09:32:15 -0400

    test2

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "This is complete 4"
-echo "this was run automagically 4"
+echo "This is complete 5"
+echo "this was run automagically 5"
